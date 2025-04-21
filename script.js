b7b91066af916573f267559f4949bf7ff151bbe1
swawiye <sovereign.wawiye@gmail.com>
2025-04-21 16:38:40 +0300

    styling

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburgerButton = document.getElementById('hamburger-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+hamburgerButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
