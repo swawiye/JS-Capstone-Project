1010522e3107d5ef169fda52564530da8de70587
swawiye <sovereign.wawiye@gmail.com>
2025-04-22 12:21:21 +0300

    functional audio controls

--- script.js
+++ script.js
@@ -8,5 +8,29 @@ hamburgerButton.addEventListener('click', () => {
 
 //Music toggle buttons
 let progress = document.getElementById("progress");
-let song = document.getElementById("progress");
-let ctrlIcon = document.getElementById("progress");
+let song = document.getElementById("song");
+let ctrlIcon = document.getElementById("ctrlIcon");
+
+song.onloadedmetadata = function () {
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+};
+
+//Toggle play and pause button
+function playPause() {
+   if(ctrlIcon.classList.contains("fa-pause")) {
+    song.pause();
+    ctrlIcon.classList.remove("fa-pause");
+    ctrlIcon.classList.add("fa-play");
+   }
+   else {
+    song.play();
+    ctrlIcon.classList.add("fa-pause");
+    ctrlIcon.classList.remove("fa-play");
+   };
+};
+
+if(song.play()) {
+    
+}
+
