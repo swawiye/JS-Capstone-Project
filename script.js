c839ed3f27ed33e10d0a24acc98ee9839c71aa5d
swawiye <sovereign.wawiye@gmail.com>
2025-04-22 11:54:15 +0300

    audio functionality

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ const mobileMenu = document.getElementById('mobile-menu');
 hamburgerButton.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden');
 });
+
+//Music toggle buttons
+let progress = document.getElementById("progress");
+let song = document.getElementById("progress");
+let ctrlIcon = document.getElementById("progress");
